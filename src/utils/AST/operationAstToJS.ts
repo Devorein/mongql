f1bf9f3e0624b5ba415f70f2df620d12ad76d904
Devon Reid <fireborn1003@gmail.com>
2020-08-18 21:44:03 +0600

    FIX: Empty fragment removal

--- src/utils/AST/operationAstToJS.ts
+++ src/utils/AST/operationAstToJS.ts
@@ -14,47 +14,75 @@ export default function operationAstToJS(OperationNodes: DocumentNode, Fragments
   let OperationOutput = 'const Operations = {};\n';
   const EmptyFragmentMap: any = {};
   const FlattenedFragmentsInfoMap: any = {};
-
+  const GeneratedFragmentsMap: any = {};
+  const DependentFragments: { dependency: string, dependents: string[] }[] = [];
 
   Object.entries(FragmentsInfoMap).forEach(([FragmentName, FragmentInfo]) => {
     Object.entries(FragmentInfo).forEach(([FragmentPartName, FragmentPartRel]) => {
       if (FragmentPartRel === false)
-        EmptyFragmentMap[`${FragmentName}${FragmentPartName}`] = false
+        EmptyFragmentMap[`${FragmentName}${FragmentPartName}Fragment`] = false
       FlattenedFragmentsInfoMap[`${FragmentName}${FragmentPartName}Fragment`] = FragmentPartRel !== false ? FragmentName + FragmentPartRel : FragmentPartRel;
     })
   });
-
-  const ExportedDefinitions: Record<string, { source: string, fragments: string[], kind: string }>[] = [{}, {}];
-  (OperationNodes.definitions as SelectionSetDefinitionNodes).forEach((Node: SelectionSetDefinitionNode) => {
+  type TExport = { nodename: string, source: string, fragments: string[], kind: string };
+  const ExportedDefinitions: { operations: TExport[], fragments: TExport[] } = { fragments: [], operations: [] };
+  (OperationNodes.definitions as SelectionSetDefinitionNodes).reverse().forEach((Node: SelectionSetDefinitionNode) => {
     const FragmentsUsed: string[] = extractFragments(Node.selectionSet);
     const DefinitionString = print(Node).split("\n").join("\n\t");
     const NodeName = (Node.name as NameNode).value;
     if (Node.kind === "FragmentDefinition") {
       if (FragmentsUsed.length > 0)
-        ExportedDefinitions[0][NodeName] = {
+        ExportedDefinitions.fragments.push({
           source: DefinitionString,
           fragments: FragmentsUsed,
-          kind: Node.kind
-        }
+          kind: Node.kind,
+          nodename: NodeName
+        })
       const mapped_fragment = FlattenedFragmentsInfoMap[NodeName];
       if (mapped_fragment) {
         const same_fragment = mapped_fragment + "Fragment" === NodeName;
         const contains_nested_fragments = FragmentsUsed.length === 0;
-        OperationOutput += (contains_nested_fragments ? `Operations.${NodeName} = ` : `const ${NodeName} = `) + (same_fragment ? `\`\n\t${DefinitionString}\`;\n\n` : `${contains_nested_fragments ? "Operations." : ""}${mapped_fragment}Fragment;\n\n`);
+        let GeneratedCode = contains_nested_fragments ? `Operations.${NodeName} = ` : `const ${NodeName} = `;
+        if (same_fragment) {
+          GeneratedFragmentsMap[NodeName] = true;
+          GeneratedCode += `\`\n\t${DefinitionString}\`;\n\n`;
+          OperationOutput += GeneratedCode;
+        }
+        else {
+          const isDependencyGenerated = Boolean(GeneratedFragmentsMap[mapped_fragment]);
+          if (!isDependencyGenerated)
+            DependentFragments.push({ dependency: mapped_fragment + "Fragment", dependents: [NodeName] });
+          else {
+            GeneratedFragmentsMap[NodeName] = true;
+            GeneratedCode += `${contains_nested_fragments ? "Operations." : ""}${mapped_fragment}Fragment;\n\n`;
+            OperationOutput += GeneratedCode;
+          }
+        }
       }
     }
     else if (Node.kind === "OperationDefinition") {
-      ExportedDefinitions[1][NodeName] = {
-        source: DefinitionString,
-        fragments: FragmentsUsed,
-        kind: Node.kind
-      }
+      ExportedDefinitions.operations.push(
+        {
+          source: DefinitionString,
+          fragments: FragmentsUsed,
+          kind: Node.kind,
+          nodename: NodeName
+        }
+      )
     }
   });
 
-  ExportedDefinitions.forEach((Definitions) => {
-    Object.entries(Definitions).forEach(([DefinitionName, DefinitionInfo]) => {
-      OperationOutput += `\nOperations.${DefinitionName} = \`\n\t${DefinitionInfo.kind === "OperationDefinition" ? DefinitionInfo.source : "${" + DefinitionName + "}"}\n${DefinitionInfo.fragments.reduce((acc, cur) => acc + `\t\${${"Operations." + cur}}\n`, '')} \`;\n`
+  DependentFragments.forEach(({ dependency, dependents }) => {
+    dependents.forEach(dependent => {
+      OperationOutput += `Operations.${dependent} = Operations.${dependency};\n\n`
+    })
+  });
+
+  // ExportedDefinitions.fragments = ExportedDefinitions.fragments.reverse();
+
+  Object.values(ExportedDefinitions).forEach((Nodes) => {
+    Nodes.forEach(Node => {
+      OperationOutput += `\nOperations.${Node.nodename} = \`\n\t${Node.kind === "OperationDefinition" ? Node.source : "${" + Node.nodename + "}"}\n${Node.fragments.reduce((acc, cur) => acc + (EmptyFragmentMap[cur] !== false ? `\t\${${"Operations." + cur}}\n` : ''), '')} \`;\n`
     });
   })
 
